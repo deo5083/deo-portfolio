46b463e279d66fa8ca3cd0584862c5ad8ebe5499
deo5083 <42614246+deo5083@users.noreply.github.com>
2019-02-01 13:46:18 -0500

    Add files via upload

---
+++ Education.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication2.Models;
+
+namespace WebApplication2
+{
+    public partial class Education : System.Web.UI.Page
+    {
+        Storage storage;
+        List<Degree> degreeList;
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string degreeFilePath = Server.MapPath("/App_Data/degrees.txt");
+
+            storage = new Storage(degreeFilePath);
+            degreeList = storage.getDegreeList();
+
+            PopulateGridView(degreeList);
+            
+
+        }
+
+        private void PopulateGridView(List<Degree> degreeList)
+        {
+
+
+            GridView_Education.DataSource = degreeList;
+            GridView_Education.DataBind();
+
+
+        }
+
+
+        private static DataTable ConvertToDatatable<T>(List<T> data)
+        {
+            PropertyDescriptorCollection props = TypeDescriptor.GetProperties(typeof(T));
+            DataTable table = new DataTable();
+            for (int i = 0; i < props.Count; i++)
+            {
+                PropertyDescriptor prop = props[i];
+                if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    table.Columns.Add(prop.Name, prop.PropertyType.GetGenericArguments()[0]);
+                else
+                    table.Columns.Add(prop.Name, prop.PropertyType);
+            }
+
+            object[] values = new object[props.Count];
+            foreach (T item in data)
+            {
+                for (int i = 0; i < values.Length; i++)
+                {
+                    values[i] = props[i].GetValue(item);
+                }
+                table.Rows.Add(values);
+            }
+            return table;
+        }
+
+    }
+}
---
+++ default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2.Models
+{
+    public partial class index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BTN_Resume_Click(object sender, EventArgs e)
+        {
+            TestSavedPDF("Resume_DavidOrtiz.pdf");
+        }
+
+        private void TestSavedPDF(string path)
+        {
+            WebClient User = new WebClient();
+
+            Byte[] FileBuffer = User.DownloadData(path);
+
+            if (FileBuffer != null)
+
+            {
+                Response.ContentType = "application/pdf";
+
+                Response.AddHeader("content-length", FileBuffer.Length.ToString());
+
+                Response.BinaryWrite(FileBuffer);
+            }
+        }
+    }
+}
